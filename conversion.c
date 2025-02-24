9ffde6d7784f43df60a01a20f8eef132dccce5fa
hima-mudigondla <hima.mudigondla@gmail.com>
2025-02-24 11:34:46 +0530

    Conversion of a Number

---
+++ conversion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number:\n");
+	scanf("%d",&n);
+	printf("Decimal:%d\n",n);
+	printf("Hexadecimal:%x\n",n);
+	printf("Hexadecimal:%X\n",n);
+	printf("Octal:%o\n",n);
+	return 0;
+}
+
