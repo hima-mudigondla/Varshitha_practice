2bc8d859e48cd57e847362d6797fb709f9a3eeeb
hima-mudigondla <hima.mudigondla@gmail.com>
2025-06-10 14:14:09 +0530

    Programs on bitwise operators

---
+++ bitwiseop/toggle_range.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,left,right;
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    printf("Enter the right anf left values:\n");
+    scanf("%d%d",&right,&left);
+    n^=((0xFFFFFFFF>>(32-right)-1)&(0xFFFFFFFF<<left));
+    printf("Result:%d\n",n);
+    return 0;
+}
