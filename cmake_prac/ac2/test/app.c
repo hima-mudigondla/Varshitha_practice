3300cee70a0a4605b3a5fe12fa4aa054ee4b66c4
hima-mudigondla <hima.mudigondla@gmail.com>
2025-10-10 11:57:00 +0530

    Makefile Cmake Autoconfig

---
+++ cmake_prac/ac2/test/app.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"math.h"
+int main()
+{
+	printf("Add 2 numbers:%d\n",add(8,4));
+	printf("Subtract 2 numbers:%d\n",sub(8,4));
+	return 0;
+}
+
---
+++ cmake_prac/cmake3/test/app.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"math.h"
+int main()
+{
+	printf("Add 2 numbers:%d\n",add(9,3));
+	printf("Subtract 2 numbers:%d\n",sub(9,3));
+	printf("Multiply 2 numbers:%d\n",mul(9,3));
+	return 0;
+}
+
