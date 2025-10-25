d25c5f4e9ec274e3dac2f348e2d4fcf4df0c4486
hima-mudigondla <hima.mudigondla@gmail.com>
2025-10-25 10:46:14 +0530

    Program on counting words,lines,characters in a file

---
+++ file_handling/count_contents.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	char filename[256],ch;
+	int characters=0,lines=1,words=1;
+	printf("Enter the filename:\n");
+	scanf("%s",filename);
+	FILE *fp=fopen(filename,"r");
+	if(fp==NULL)
+	{
+		printf("File cannot be opened!\n");
+		return 1;
+	}
+	while((ch=fgetc(fp))!=EOF)
+	{
+		characters++;
+		if(ch=='\n')
+		{
+			lines++;
+		}
+		if(ch==' ')
+		{
+			words++;
+		}
+	}
+	printf("Number of characters:%d\n",characters);
+	printf("Number of words:%d\n",words);
+	printf("Number of lines:%d\n",lines);
+	fclose(fp);
+	return 0;
+}
+
+
